17b745fa7011fbefb0710798850e76a3238b5091
Vaibhav Kolte <vaibhavkolte2005@gmail.com>
2026-03-08 00:32:17 +0530

    change url path improved navbar and home page styling

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-// const DEFAULT_API_URL = "https://e-commarce-backend-1.onrender.com/api";
+const DEFAULT_API_URL = "https://e-commarce-backend-1.onrender.com/api";
 
-const DEFAULT_API_URL = "http://localhost:8080/api"
+// const DEFAULT_API_URL = "http://localhost:8080/api"
 const BASE_URL = import.meta.env.DEV
     ? '/api'
     : (import.meta.env.VITE_API_URL || DEFAULT_API_URL);
 
 const api = axios.create({
     // baseURL: BASE_URL,
-    baseURL: "http://localhost:8080/api",
+    baseURL: DEFAULT_API_URL,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
